202af0a40567a9439dce67d7f80064548dbbd4a6
sovernut <orenomail4@gmail.com>
2021-04-23 15:00:20 +0700

    submit form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import classes from './AddUser.module.css'
 
 
 const AddUser = props => {
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(username,age)
+    }
+
+    const onChangeUsername = (event) => {
+        setUsername(event.target.value)
+    }
+    const onChangeAge = (event) => {
+        setAge(event.target.value)
     }
 
     return (
         <Card classNameJa={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text"/>
+                <input onChange={onChangeUsername} id="username" type="text"/>
                 <label htmlFor="age">Age (years)</label>
-                <input id="age" type="number"/>
+                <input onChange={onChangeAge} id="age" type="number"/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
