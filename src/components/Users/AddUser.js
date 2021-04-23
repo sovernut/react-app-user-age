28af38cb68ae15c30f4440af14351abe7f76d411
sovernut <orenomail4@gmail.com>
2021-04-23 15:04:01 +0700

    simple validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,7 +10,15 @@ const AddUser = props => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (username.trim().length === 0 || age.trim().length === 0) {
+            return
+        }
+        if (+age < 1){
+            return
+        }
         console.log(username,age)
+        setUsername('')
+        setAge('')
     }
 
     const onChangeUsername = (event) => {
@@ -24,9 +32,9 @@ const AddUser = props => {
         <Card classNameJa={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input onChange={onChangeUsername} id="username" type="text"/>
+                <input onChange={onChangeUsername} id="username" type="text" value={username}/>
                 <label htmlFor="age">Age (years)</label>
-                <input onChange={onChangeAge} id="age" type="number"/>
+                <input onChange={onChangeAge} id="age" type="number" value={age}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
