2f762cdb6839c2ba4ea97f76bc5c6b65d2c5a8ca
sovernut <orenomail4@gmail.com>
2021-04-24 12:17:31 +0700

    working with portal and fragment

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import ErrorModal from '../UI/ErrorModal'
 import classes from './AddUser.module.css'
+import Wrapper from '../Helpers/Wrapper'
 
 
 const AddUser = props => {
@@ -19,14 +20,14 @@ const AddUser = props => {
             })
             return
         }
-        if (+age < 1){
+        if (+age < 1) {
             setError({
                 title: 'invalid age',
                 message: 'Please enter a valid age (>0))'
             })
             return
         }
-        console.log(username,age)
+        console.log(username, age)
         props.onAddUser(username, age, Math.random().toString())
         setUsername('')
         setAge('')
@@ -42,18 +43,19 @@ const AddUser = props => {
         setError(null)
     }
 
-    return (<div>
-        {error && <ErrorModal title={error.title} message={error.message} confirm={errorHandler}/>}
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input onChange={onChangeUsername} id="username" type="text" value={username}/>
-                <label htmlFor="age">Age (years)</label>
-                <input onChange={onChangeAge} id="age" type="number" value={age}/>
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
-        </div>
+    return (
+        <Wrapper>
+            {error && <ErrorModal title={error.title} message={error.message} confirm={errorHandler} />}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input onChange={onChangeUsername} id="username" type="text" value={username} />
+                    <label htmlFor="age">Age (years)</label>
+                    <input onChange={onChangeAge} id="age" type="number" value={age} />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </Wrapper>
     )
 }
 
